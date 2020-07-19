5e32dcba47386a47dc2c4f9ecfdc096494b9d8bf
nitigan.n <nitigan.n@fitm.kmutnb.ac.th>
2020-07-20 02:49:44 +0700

    new Surname

--- test.cpp
+++ test.cpp
@@ -2,10 +2,12 @@
 #include<string>
 using namespace std;
 int main(){
-    string name;
+    string name,surname;
     cout << "Enter Name : ";
     cin >> name;
-    cout <<"My name is " << name <<endl;
+    cout << "Enter SurName : ";
+    cin >> surname;
+    cout <<"My name is " << name <<" "<< surname <<endl;
     system("pause");
     return 0;
 }

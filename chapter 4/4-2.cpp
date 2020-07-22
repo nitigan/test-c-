3417346f11e8a77adcc3b0970add5e53f2a60438
nitigan.n <nitigan.n@fitm.kmutnb.ac.th>
2020-07-22 23:07:08 +0700

    update chapter 4

---
+++ chapter 4/4-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float X;
+    cout << "Enter value X : ";
+    cin >> X ;
+    float Result (0);
+    Result = X * X * X + 3 * X - 10;
+    cout << "\nResult of X*X*X + 3*X - 10 = ";
+    cout << Result << endl;
+    system("pause");
+    return (0);
+}
